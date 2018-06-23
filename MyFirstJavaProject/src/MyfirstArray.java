3e2c1d1e69de35ee62b5b473e114d86bfcd1430c
praka <praka@DESKTOP-KKNELTV>
2018-06-23 12:46:49 -0700

    First GitHub upload

---
+++ MyFirstJavaProject/src/MyfirstArray.java
@@ -0,0 +1,18 @@
+
+public class MyfirstArray {
+
+	public static void main(String[] args) {
+		int multidimension[][] = {{5,6},{9,10}};
+		
+		int i,j;
+		for(i=0;i<2;i++) {
+			for(j=0;j<2;j++) {
+				System.out.print(multidimension[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
---
+++ MyFirstJavaProject/src/Myfirstcode.java
@@ -0,0 +1,15 @@
+
+public class Myfirstcode {
+
+
+
+	public static void main(String[] args) 
+	{
+		char Name = 'P' ;
+		String str = new String("Prakash.");
+		System.out.println( "My Name Start with Character :" + Name);
+		System.out.println( "And its;" + str);
+		
+	}
+
+}
